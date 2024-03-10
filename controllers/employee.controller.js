d98f47a444659e590d027a8947d6af10a9e1a999
faisal <mahmudrifat200@gmail.com>
2024-03-10 17:13:05 +0600

    get post api check

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -16,10 +16,67 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/getAllUserDetails',async (req,res) => {
+    const userDetails = await service.getAllUserDetails()
+
+    console.log('userDetailsLog',userDetails);
+
+    if(userDetails ==  undefined){
+        res.status(404).json('No data has been found')
+    }
+    else{
+        res.send(userDetails)
+    }
+
+})
+
+router.get('/getAllOwnerDetails',async (req,res) => {
+    if(req.query.userType){
+        
+        const userDetails = await service.getAllOwnerDetails(req.query.userType)
+        
+        if(userDetails ==  undefined){
+            res.status(404).json('No data has been found')
+        }
+        else{
+            res.send(userDetails)
+        }
+    }
+    else{
+        const errorResponse = {
+            status: '2000',
+            errorMessage: 'Please insert proper userType'
+        }
+        res.status(404).json(errorResponse)
+    }
+
+})
+router.get('/getOwnerUserDetailsById',async (req,res) => {
+    if(req.query.userDetailsId && req.query.userType){
+        
+        const userDetails = await service.getOwnerUserDetailsById(req.query.userType,req.query.userDetailsId)
+        
+        if(userDetails ==  undefined){
+            res.status(404).json('No data has been found')
+        }
+        else{
+            res.send(userDetails)
+        }
+    }
+    else{
+        const errorResponse = {
+            status: '2000',
+            errorMessage: 'Please insert proper userDetailsId, userType'
+        }
+        res.status(404).json(errorResponse)
+    }
+
+})
+
 router.get('/:id', async (req, res) => {
     const employee = await service.getEmployeeById(req.params.id)
     if (employee == undefined)
-        res.status(404).json('no record with given id : ' + req.params.id)
+        res.status(404).json('no record with given id1 : ' + req.params.id)
     else
         res.send(employee)
 })
@@ -27,20 +84,32 @@ router.get('/:id', async (req, res) => {
 router.delete('/:id', async (req, res) => {
     const affectedRows = await service.deleteEmployee(req.params.id)
     if (affectedRows == 0)
-        res.status(404).json('no record with given id : ' + req.params.id)
+        res.status(404).json('no record with given id2 : ' + req.params.id)
     else
         res.send('deleted successfully.')
 })
 
 router.post('/', async (req, res) => {
-    await service.addOrEditEmployee(req.body)
-    res.status(201).send('created successfully.')
+    try {
+        await service.addOrEditEmployee(req.body)
+    
+        res.status(201).send('created successfully.')
+    } catch (error) {
+        
+        console.log('errorofAdd',error);
+        const errorResponse = {
+            status: error.errno,
+            errorMessage: error?.sqlMessage
+        }
+        res.status(404).send(errorResponse)
+
+    }
 })
 
 router.put('/:id', async (req, res) => {
     const affectedRows = await service.addOrEditEmployee(req.body, req.params.id)
     if (affectedRows == 0)
-        res.status(404).json('no record with given id : ' + req.params.id)
+        res.status(404).json('no record with given id 3: ' + req.params.id)
     else
         res.send('updated successfully.')
 })
--- services/employee.service.js
+++ services/employee.service.js
@@ -25,3 +25,20 @@ module.exports.getEmployeeByEmpCode = async (EmpCode) => {
     const [[record]] = await db.query("SELECT * FROM employees WHERE employee_code = ?", [EmpCode])
     return record
 }
+
+module.exports.getAllUserDetails = async () => {
+    const [record] = await db.query("SELECT * FROM user_details LIMIT 200")
+    console.log('records',record);
+    return record
+}
+
+module.exports.getAllOwnerDetails = async (userType) => {
+    const [record] = await db.query("SELECT * FROM user_details where user_type = ? LIMIT 200", [userType])
+    return record
+}
+
+module.exports.getOwnerUserDetailsById = async (userType,userDetailsId) => {
+    const [record] = await db.query("SELECT full_name, user_id, user_details_id, email FROM user_details where user_type = ? and user_details_id = ?", [userType,userDetailsId])
+    console.log('record',record);
+    return record
+}
